b8b4223154fcc4cb0be8207eb2b186b2d4f384a5
felipecrdos <felipecrdos@gmail.com>
2020-11-16 06:24:24 -0300

    add data.json/validation fields/save and load data

---
+++ Global.gd
@@ -0,0 +1,31 @@
+extends Node
+
+# Variables
+var data_path = "res://data/data.json"
+
+# Initializer
+func _ready():
+	print(load_data())
+	
+# Save data
+func save_data(data):
+	var file = File.new()
+	file.open(data_path, File.WRITE)
+	file.store_line(to_json(data))
+	file.close()
+
+# Add asteroid
+func add_asteroid(data):
+	var datas = load_data()
+	datas['asteroids'].append(data)
+	save_data(datas)
+	
+# Load data
+func load_data():
+	var file = File.new()
+	if !file.file_exists(data_path):
+		return
+	file.open(data_path, File.READ)
+	var data = parse_json(file.get_as_text())
+	file.close()
+	return data
--- scripts/Register.gd
+++ scripts/Register.gd
@@ -1,8 +1,95 @@
 extends Control
 
+var _name
+var _diameter
+var _distance
+var _discoverer
 
+var _name_label
+var _diameter_label
+var _distance_label
+var _discoverer_label
+
+var alert_color
+var default_color
 func _ready():
-	pass
+	_name 		= $VBMain/MTop/VBTop/HBName/TextEdit
+	_diameter 	= $VBMain/MTop/VBTop/HBDiameter/TextEdit
+	_distance 	= $VBMain/MTop/VBTop/HBDistance/TextEdit
+	_discoverer = $VBMain/MTop/VBTop/HBDiscoverer/TextEdit
+	
+	_name_label = $VBMain/MTop/VBTop/HBName/Label
+	_diameter_label = $VBMain/MTop/VBTop/HBDiameter/Label
+	_distance_label = $VBMain/MTop/VBTop/HBDistance/Label
+	_discoverer_label = $VBMain/MTop/VBTop/HBDiscoverer/Label
+	
+	alert_color = Color("#fe7272")
+	default_color = Color("#ffffff")
+	
+func clear_fields():
+	_name.text = ''
+	_diameter.text = ''
+	_distance.text = ''
+	_discoverer.text = ''
 
+func empty_fields():
+	var is_empty = false
+	if _name.text == '':
+		if not "type name" in _name_label.text:
+			_name_label.text = "type name"
+		set_label_color(_name_label, alert_color)
+		is_empty = true
+	if _diameter.text == '':
+		if not "type diameter" in _diameter_label.text:
+			_diameter_label.text = "type diameter"
+		set_label_color(_diameter_label, alert_color)
+		is_empty = true
+	if _distance.text == '':
+		if not "type distance" in _distance_label.text:
+			_distance_label.text = "type distance"
+		set_label_color(_distance_label, alert_color)
+		is_empty = true
+	if _discoverer.text == '':
+		if not "type discoverer" in _discoverer_label.text:
+			_discoverer_label.text = "type discoverer"
+		set_label_color(_discoverer_label, alert_color)
+		is_empty = true
+		
+	return is_empty
+	
 func _on_Back_send_scene(scene):
 	get_tree().change_scene(scene)
+
+func _on_Save_send_scene(scene):
+	if !empty_fields():
+		var asteroid = {
+			"name": _name.text,
+			"diamter": _diameter.text,
+			"distance": _distance.text,
+			"discoverer": _discoverer.text
+		}
+		Global.add_asteroid(asteroid)
+		clear_fields()
+
+func set_label_color(label, color):
+	label.set("custom_colors/font_color", color)
+
+
+func _on_Name_text_changed():
+	_name_label.text = "name"
+	set_label_color(_name_label, default_color) 
+
+
+func _on_Diameter_text_changed():
+	_diameter_label.text = "diameter"
+	set_label_color(_diameter_label, default_color)
+
+
+func _on_Distance_text_changed():
+	_distance_label.text = "distance"
+	set_label_color(_distance_label, default_color)
+
+
+func _on_Discoverer_text_changed():
+	_discoverer_label.text = "discoverer"
+	set_label_color(_discoverer_label, default_color)
