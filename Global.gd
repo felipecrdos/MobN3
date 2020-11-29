80ba98c1585cf6f59655a16da772ccd35ad797d2
felipecrdos <felipecrdos@gmail.com>
2020-11-29 19:39:15 -0300

    change save path

--- Global.gd
+++ Global.gd
@@ -1,7 +1,17 @@
 extends Node
 
 # Variables
-var data_path = "res://data/data.json"
+var data_path = "user://data.json"
+
+# default values
+var default_values : Dictionary = {
+	"asteroids":[
+		{"diameter":934.5,"discoverer":"Giuseppe Piazzi","distance":"256 AU","name":"Ceres"},
+		{"diameter":525.4,"discoverer":"Heinrich Wilhelm Matthias Olbers","distance":"2.362 AU","name":"Vesta"},
+		{"diameter":512,"discoverer":"Heinrich Wilhelm Matthias Olbers","distance":"2.773 AU","name":"Pallas"},
+		{"diameter":534,"discoverer":"Annibale de Gasparis","distance":"3.139 AU","name":"Hygiea"}
+		]
+	}
 
 # Save data
 func save_data(data):
@@ -20,8 +30,10 @@ func add_asteroid(data):
 func load_data():
 	var file = File.new()
 	if !file.file_exists(data_path):
-		return
+		save_data(default_values)
 	file.open(data_path, File.READ)
 	var data = parse_json(file.get_as_text())
 	file.close()
 	return data
+
+
